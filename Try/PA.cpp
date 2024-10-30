21dc697a4a9f289fb2a2318687c9637f52b56b99
Pirate-Emperor <piratekingrahul@gmail.com>
2024-10-31 01:25:54 +0530

    31-10-24

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -27,52 +27,12 @@ ll qexp(ll a, ll b, ll m) {
 void solve() {
     ll n,q,x,y,k;
     cin >> n;
-    vector<ll> arr(n+3,0),pref(n+3,0),suf(n+3,0),sufs(n+3,0),ind(n+3,LINF),sufa(n+3,0),resp(n+100,0);
+    vector<ll> arr(n,0);
     for (int i=0;i<n;i++)
     {
         cin >> arr[i];
-        pref[i+1]=pref[i]+arr[i];
     }
-    for (int i=n-1;i>=0;i--)
-    {
-        suf[i]=suf[i+1]+arr[i];
-        sufs[i]=sufs[i+1]+suf[i];
-        sufa[i]=sufa[i+1]+arr[i]*(n-i);
-    }
-    for (int i=1;i<=n;i++)
-    {
-        resp[i]=resp[i-1]+sufa[i-1];
-    }
-    ind[0]=0;
-    for (int i=0;i<n;i++)
-    {
-        ind[i+1]=ind[i]+n-i;
-    }
-    cin >> q;
-    for (int i=0;i<q;i++)
-    {
-        cin >> x >> y;
-        x--;
-        y--;
-        auto prefsum = [&](ll y){
-            ll u = upper_bound(ind.begin(),ind.end(),y) - ind.begin();
-            u--;
-            ll pr = resp[u];
-            ll ti = u+y-ind[u]+2;
-            ll tu = sufa[u] + sufs[ti] - suf[u]*(n-ti+1);
-            // cout << u << " " << pr << " " << ti << " " << sufa[u] << " " << sufs[ti] << " " << suf[u] << " " << tu << " " << endl;
-            pr+=tu;
-            return pr;
-        };
-        ll resu = prefsum(y);
-        if (x==0)
-        {
-            cout << resu << endl;
-            continue;
-        }
-        resu-=prefsum(x-1);
-        cout << resu << endl;
-    }
+    
     return;
 }
 
