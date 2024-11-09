a73e3dc70707a6c2f4d372ae67a978206e431ce8
Pirate-Emperor <piratekingrahul@gmail.com>
2024-11-09 23:57:57 +0530

    09-11-24

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -24,44 +24,86 @@ ll qexp(ll a, ll b, ll m) {
 }
 
 void solve() {
+    ll n=0,m,q=0,resu=0,on=0,ze=0,a=0,b=0,c=0,d;
     cin >> n >> m;
-    vector<ll> brr(n+1);
-    ll sum=0;
-    for (int i = 1; i <= n; i++) 
+    vector<map<ll,ll>> adj(n);
+    for (int i=0;i<m;i++)
     {
-        cin >> brr[i];
-        brr[i]%=m;
-        ll temp = brr[i]*(i)*(n-i+1);
-        sum+=temp;
-        arr[i]=0;
+        cin >> a >> b;
+        a--;
+        b--;
+        adj[a].insert({b,1});
+        adj[b].insert({a,1});
     }
-    for (int i=0;i<=m;i++) arr[i]=0;
-    build(1, 1, m);
-    int idx = m-1;
-    ll resu=0;
-    for (int i=1;i<=n;i++) 
+    resu=2*max(n,m);
+    ll lef=resu;
+    vector<ar<ll,3>> res;
+    for (int i=0;i<n;i++)
     {
-        if (brr[i]==0)
-        {
-            update(1, 1, n, idx+1, idx+1, 1, 1);
-            continue;
+        c=adj[i].size();
+        if (c<=1) continue;
+        auto tem = adj[i];
+        for (auto it = tem.rbegin(); it != tem.rend(); advance(it, 1)) {
+            auto fi = it;
+            advance(it,1);
+            if (it==tem.rend()) break;
+            auto si = it;
+            if (adj[fi->first].find(si->first)!=adj[fi->first].end())
+            {
+                adj[fi->first].erase(si->first);
+                adj[si->first].erase(fi->first);
+            }
+            else
+            {
+                adj[fi->first][si->first]=1;
+                adj[si->first][fi->first]=1;
+            }
+            adj[fi->first].erase(i);
+            adj[si->first].erase(i);
+            adj[i].erase(fi->first);
+            adj[i].erase(si->first);
+            lef--;
+            res.push_back({i,fi->first,si->first});
+            // cout << i << fi->first << si->first << " ";
         }
-        if (idx>brr[i])
-        {
-            resu+=query(1, 1, n, idx-brr[i]+2, idx+1).sum;
-        }
-        else
-        {
-            resu+=query(1, 1, n, 1, idx+1).sum;
-            resu+=query(1, 1, n, (idx-brr[i]+1+m)%m+1, m).sum;
-        }
-        idx = (m-brr[i]+idx)%m; 
-        update(1, 1, n, idx+1, idx+1, 1, 1);
-
     }
-    cout << resu << endl;
-    resu = sum - resu*m;
-    cout << resu << endl;
+    vector<bool> vis(n);
+    vector<ll> temp;
+    a=-1;
+    b=-1;
+    for (int i=0;i<n;i++)
+    {
+        if (!vis[i])
+        {
+            vis[i]=true;
+            temp.push_back(i);
+            if (adj[i].size()>0)
+            {
+                int j=adj[i].begin()->first;
+                vis[j]=true;
+                a=i;
+                b=j;
+            }
+        }
+        
+    }
+    if (a!=-1)
+    {
+        for (int i=0;i<temp.size();i++)
+        {
+            if (a==temp[i]) continue;
+            res.push_back({a,b,temp[i]});
+            b=temp[i];
+        }
+    }
+    cout << res.size() << endl;
+    for (int i=0;i<res.size();i++)
+    {
+        a = res[i][0]+1;
+        b = res[i][1]+1;
+        c = res[i][2]+1;
+        cout << a << " " << b << " " << c << endl;
+    }
     return;
 }
 
@@ -77,7 +119,7 @@ int main() {
     // freopen("output.txt", "w", stdout);
 
     int tc; tc = 1;
-    // cin >> tc;
+    cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t  << ": ";
         solve();
