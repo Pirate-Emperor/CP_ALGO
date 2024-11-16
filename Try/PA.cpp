cfd8fd2dfaf8422f24713c54383ccf8788b54871
Pirate-Emperor <piratekingrahul@gmail.com>
2024-11-16 19:49:27 +0530

    16/11/24

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ar array
 #define ll long long
 
-const int MAX_N = 2e5 + 1;
+const int MAX_N = 5e5 + 5;
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
 const ll LINF = 1e18;
@@ -23,46 +23,81 @@ ll qexp(ll a, ll b, ll m) {
     return res;
 }
 
+vector<ll> arr,brr,crr;
+ll moves=0;
+ll func(ll cur, ll a, ll b)
+{
+    return (cur==0)?max(a,b):min(a,b);
+}
+bool recur(ll cur)
+{
+    // if (moves>=3) return 1-cur;
+    if (cur==0 && arr.size()==0) return false;
+    else if (cur==1 && brr.size()==0) return false;
+    if (cur==0)
+    {
+        auto tem = arr;
+        for (int i=0;i<(int)tem.size();i++)
+        {
+            for (int j=0;j<(int)crr.size();j++)
+            {
+                if (crr[j]<arr[i]) 
+                {
+                    swap(arr[i],crr[j]);
+                    if (recur(1-cur)==false) return true;
+                    moves++;
+                    swap(arr[i],crr[j]);
+                }
+            }
+            crr.push_back(tem[i]);
+            arr.erase(arr.begin()+i);
+            if (recur(1-cur)==false) return true;
+            moves++;
+            arr.insert(arr.begin()+i,1,tem[i]);
+            crr.pop_back();
+        }
+    }
+    else
+    {
+        auto tem = brr;
+        for (int i=0;i<(int)tem.size();i++)
+        {
+            for (int j=0;j<(int)crr.size();j++)
+            {
+                if (crr[j]<brr[i]) 
+                {
+                    swap(brr[i],crr[j]);
+                    if (recur(1-cur)==false) return true;
+                    moves++;
+                    swap(brr[i],crr[j]);
+                }
+            }
+            crr.push_back(tem[i]);
+            brr.erase(brr.begin()+i);
+            
+            if (recur(1-cur)==false) return true;
+            moves++;
+            brr.insert(brr.begin()+i,1,tem[i]);
+            crr.pop_back();
+        }
+    }
+    // 
+    return false;
+}
+
+
 void solve() {
-    ll n=0,m,q=0,resu=0,on=0,ze=0,a=0,b=0,c=0,x;
-    cin >> n;
-    vector<ll> arr(n),pref(n+1,0),nex(n+1,-1),res(n+1,0);
-    map<pair<ll,ll>,ll> greq,les;
-    for (int i=0;i<n;i++)
-    {
-        cin >> arr[i];
-        pref[i+1]=max(pref[i],arr[i]);
-        greq[{arr[i],i}]=1;
-    }
-    a=0;
-    for (int i=0;i<n;i++)
-    {
-        map<pair<ll,ll>,ll> tem;
-        auto it = greq.begin();
-        while(it != greq.end() && it->first.first<arr[i])
-        {
-            tem[{it->first.first,it->first.second}]=1;
-            les[{it->first.second,it->first.first}]=1;
-            it++;
-        }
-        for (auto it2=tem.begin();it2!=tem.end();it2++)
-        {
-            greq.erase(it2->first);
-        }
-        nex[i]=i;
-        if (les.rbegin()!=les.rend()) nex[i]=max(nex[i],les.rbegin()->first.first);
-    }
-    for (int i=n-1;i>=0;i--)
-    {
-        
-        res[i+1]=max(pref[i+1],res[nex[i]+1]);
-    }
-    for (int i=0;i<n;i++)
-    {
-        cout << res[i+1] << " ";
-    }
-    cout << endl;
-    return;
+    ll n=0,m=0,l=0,x=0;
+    cin >> n >> m >> l;
+    for (int i=0;i<n;i++) cin >> x, arr.push_back(x);
+    for (int i=0;i<m;i++) cin >> x, brr.push_back(x);
+    for (int i=0;i<l;i++) cin >> x, crr.push_back(x);
+
+    ll res = recur(0);
+    if (res==true) cout << "Aoki\n";
+    else cout << "Takahashi\n";
+    map<ll,ll> col;
+    
 }
 
 
@@ -77,7 +112,7 @@ int main() {
     // freopen("output.txt", "w", stdout);
 
     int tc; tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t  << ": ";
         solve();
