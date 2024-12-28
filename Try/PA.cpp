f08a770f3377c30956e23de4f74a3e30430b42e4
Pirate-Emperor <piratekingrahul@gmail.com>
2024-12-28 23:16:43 +0530

    28-12-24

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 #define all(x) (x).begin(),(x).end()
-#define ar array
+#define ari array
 #define ll long long
 #define int long long
 
 const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
+const ll MOD = 998244353;
 const ll INF = 1e9;
 const ll LINF = 1e18;
 
@@ -25,21 +25,123 @@ ll gcd(ll a, ll b) {
     return b ? gcd(b, a % b) : a;
 }
 
+ll qexp(ll a, ll b, ll m) {
+    ll res = 1;
+    while (b) {
+        if (b % 2) res = res * a % m;
+        a = a * a % m;
+        b /= 2;
+    }
+    return res;
+}
+
 void solve() {
     ll n=0,m=0,l=0,r=0,k=0;
     ll w=0,x=0,y=0,z=0;
     ll a=0,b=0,c=0,d=0;
-    ll g=0;
-    cin >> n;
-    vector<ll> arr(n);
-    for (int i=0;i<n;i++) 
+    ll g=0,q=0;
+    cin >> n >> q;
+    vector<ll> arr(n),brr(n);
+    map<ll,ll> ma,mb;
+    vector<ari<ll,2>> ar(n),br(n);
+    for (int i=0;i<n;i++)
     {
-        cin >> arr[i];
+        cin >> x;
+        arr[i]=x;
+        ma[x]++;
+        ar[i]={x,i};
+        // ar[{arr[i],i}]=1;
     }
-    
+    for (int i=0;i<n;i++)
+    {
+        cin >> x;
+        brr[i]=x;
+        mb[x]++;
+        br[i]={x,i};
+        // br[{brr[i],i}]=1;
+    }
+    map<ll,ari<ll,2>> am,bm;
+    ll ti=0;
+    for (auto it: ma)
+    {
+        am[it.first]={it.second,ti};
+        ti+=it.second;
+    }
+    ti=0;
+    for (auto it: mb)
+    {
+        bm[it.first]={it.second,ti};
+        ti+=it.second;
+    }
+    sort(ar.begin(),ar.end());
+    sort(br.begin(),br.end());
+    ll ans = 1;
+    for (int i=0;i<n;i++)
+    {
+        ans = (ans*min(ar[i][0],br[i][0]))%MOD;
+    }
+    cout << ans << " ";
+    for (int i=0;i<q;i++)
+    {
+        ll res = ans;
+        cin >> x >> y;
+        y--;
+        
+        
 
+        if (x==1)
+        {
+            ll valy = arr[y];
+            auto it1 = am[valy];
+            auto it2 = it1;
+            am.erase(valy);
 
-    
+            
+            ll oi = it1[1]+it1[0]-1;
+            ll oival = min(br[oi][0],ar[oi][0]);
+            res = (res*qexp(oival,MOD-2,MOD))%MOD;
+            
+            it1[0]--;
+            if (it1[0]>0) am[valy]=it1;
+            it2 = am[valy+1];
+            am[valy+1]={it2[0]+1,it1[0]+it1[1]};
+            arr[y]++;
+            
+            it2 = am[valy+1];
+            ll oj = it2[1];
+            ar[oj][0]=valy+1;
+            ll ojval = min(br[oj][0],ar[oj][0]);
+            res = (res*ojval)%MOD;
+            
+        }
+        else 
+        {
+            ll valy = brr[y];
+            auto it1 = bm[valy];
+            auto it2 = it1;
+            bm.erase(valy);
+
+            
+            ll oi = it1[1]+it1[0]-1;
+            ll oival = min(br[oi][0],ar[oi][0]);
+            res = (res*qexp(oival,MOD-2,MOD))%MOD;
+            
+            it1[0]--;
+            if (it1[0]>0) bm[valy]=it1;
+            it2 = bm[valy+1];
+            bm[valy+1]={it2[0]+1,it1[0]+it1[1]};
+            brr[y]++;
+            
+            it2 = bm[valy+1];
+            ll oj = it2[1];
+            br[oj][0]=valy+1;
+            ll ojval = min(br[oj][0],ar[oj][0]);
+            res = (res*ojval)%MOD;
+        }
+        ans = res;
+        cout << res << " ";
+    }
+    cout << endl;
 }
 
 signed main() {
--- Try/PE.cpp
+++ Try/PE.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 #define ar array
 #define ll long long
+#define int long long
 
-const int MAX_N = 1e6;
+const int MAX_N = 2e5+100;
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
 const ll LINF = 1e18;
@@ -16,91 +17,62 @@ const ll LINF = 1e18;
 int n, m, vis[MAX_N];
 vector<int> adj[MAX_N];
 
-
-
-ll dfs(int u) {
+void dfs(int u) {
     vis[u] = 1;
-    ll b=0,resu=0;
-    vector<ll> dep;
     for (int v : adj[u]) {
         if (vis[v]) continue;
-        b = dfs(v);
-        resu=max(resu,b);
-        dep.push_back(b);
+        dfs(v);
     }
-    if (dep.size()==0) return 0;
-    else if (dep.size()<3) return resu+1;
-    sort(dep.begin(),dep.end());
-    ll l=resu+1,r=resu+100,mid=0;
-    ll res=resu+100;
-    // cout << u << " " << dep[3] << " " << endl;
-    while(l<r)
-    {
-        mid = (r+l)/2;
-        ll curd=mid-dep[0],cnt=1;
-        for (int i=1;i<dep.size();i++)
-        {
-            while(curd>mid-dep[i])
-            {
-                cnt=(cnt+1)/2;
-                curd--;
-            }
-            cnt++;
-        }
-        ll tem = log2(cnt-1);
-        
-        if (curd<=tem) l=mid+1;
-        else 
-        {
-            // cout << mid << " " << tem << " " << curd << " " << endl;
-            res=min(res,mid);
-            r=mid-1;
-        }
-    }
-    for (ll mid: {l,r})
-    {
-        ll curd=mid-dep[0],cnt=1;
-        for (int i=1;i<dep.size();i++)
-        {
-            while(curd>mid-dep[i])
-            {
-                cnt=(cnt+1)/2;
-                curd--;
-            }
-            cnt++;
-        }
-        ll tem = log2(cnt-1);
-        
-        if (curd<=tem) l=mid+1;
-        else 
-        {
-            // cout << mid << " " << tem << " " << curd << " " << endl;
-            res=min(res,mid);
-            r=mid-1;
-        }
-    }
-    return res;
 }
 
 void solve() {
     cin >> n;
     m=n-1;
-    for (int i = 0; i < m; i++) {
-        int u, v; cin >> u;
-        v = i+2;
-        adj[u].push_back(v);
-    }
-    ll resu=0;
-    resu = dfs(1);
-    cout << resu << endl;
-    for (int i=1;i<=n;i++)
+    vector<pair<ll,ll>> edges;
+    for (int i = 1; i <= n; i++) 
     {
-        vis[i]=0;
         adj[i].clear();
     }
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        edges.push_back({u,v});
+        edges.push_back({v,u});
+    }
+    vector<ll> leafs;
+    map<ll,ll> lef,deg1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[i].size()==1) 
+        {
+            leafs.push_back(i);
+            lef[i]=1;
+            if (adj[adj[i][0]].size()!=1) deg1[adj[i][0]]=1;
+        }
+        // adj[i].clear();
+    }
+    // for (auto it: edges) {
+    //     int u = it.first;
+    //     int v = it.second;
+    //     if (lef.find(u)!=lef.end()) continue;
+    //     if (lef.find(v)!=lef.end()) continue;
+
+    //     adj[u].push_back(v);
+    //     adj[v].push_back(u);
+    // }
+    
+    ll temp = leafs.size();
+    ll res = temp*(n-temp);
+    n-=temp;
+    ll temp2 = deg1.size();
+    cout << temp << " " << temp2 << " ";
+    res += temp2*(n-temp2);
+    cout << res << endl;    
+
 }
 
-int main() {
+
+signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     // freopen("input.txt", "r", stdin);
