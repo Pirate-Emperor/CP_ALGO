d3f98e2779f071b51c42ba7a5774f29adba9011c
Pirate-Emperor <piratekingrahul@gmail.com>
2025-01-03 20:20:29 +0530

    03-01-25

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #define ll long long
 #define int long long
 
-const int MAX_N = 1e5 + 5;
+const int MAX_N = 2e5 + 5;
 const ll MOD = 998244353;
 const ll INF = 1e9;
 const ll LINF = 1e18;
@@ -34,114 +34,56 @@ ll qexp(ll a, ll b, ll m) {
     }
     return res;
 }
+vector<int> adj[MAX_N];
+int vis[MAX_N];
 
 void solve() {
     ll n=0,m=0,l=0,r=0,k=0;
     ll w=0,x=0,y=0,z=0;
     ll a=0,b=0,c=0,d=0;
     ll g=0,q=0;
-    cin >> n >> q;
-    vector<ll> arr(n),brr(n);
-    map<ll,ll> ma,mb;
-    vector<ari<ll,2>> ar(n),br(n);
-    for (int i=0;i<n;i++)
+    cin >> n;
+    vector<map<int,int>> adj(n);
+    for (int i=1;i<n;i++)
     {
-        cin >> x;
-        arr[i]=x;
-        ma[x]++;
-        ar[i]={x,i};
-        // ar[{arr[i],i}]=1;
-    }
-    for (int i=0;i<n;i++)
-    {
-        cin >> x;
-        brr[i]=x;
-        mb[x]++;
-        br[i]={x,i};
-        // br[{brr[i],i}]=1;
-    }
-    map<ll,ari<ll,2>> am,bm;
-    ll ti=0;
-    for (auto it: ma)
-    {
-        am[it.first]={it.second,ti};
-        ti+=it.second;
-    }
-    ti=0;
-    for (auto it: mb)
-    {
-        bm[it.first]={it.second,ti};
-        ti+=it.second;
-    }
-    sort(ar.begin(),ar.end());
-    sort(br.begin(),br.end());
-    ll ans = 1;
-    for (int i=0;i<n;i++)
-    {
-        ans = (ans*min(ar[i][0],br[i][0]))%MOD;
-    }
-    cout << ans << " ";
-    for (int i=0;i<q;i++)
-    {
-        ll res = ans;
         cin >> x >> y;
+        x--;
         y--;
-        
-        
-
-        if (x==1)
+        adj[x][y]=1;
+        adj[y][x]=1;
+    }    
+    vector<int> temp;
+    vector<int> up(n);
+    for (int i=0;i<n;i++)
+    {
+        if (adj[i].size()==1) 
         {
-            ll valy = arr[y];
-            auto it1 = am[valy];
-            auto it2 = it1;
-            am.erase(valy);
-
-            
-            ll oi = it1[1]+it1[0]-1;
-            ll oival = min(br[oi][0],ar[oi][0]);
-            res = (res*qexp(oival,MOD-2,MOD))%MOD;
-            
-            it1[0]--;
-            if (it1[0]>0) am[valy]=it1;
-            it2 = am[valy+1];
-            am[valy+1]={it2[0]+1,it1[0]+it1[1]};
-            arr[y]++;
-            
-            it2 = am[valy+1];
-            ll oj = it2[1];
-            ar[oj][0]=valy+1;
-            ll ojval = min(br[oj][0],ar[oj][0]);
-            res = (res*ojval)%MOD;
-            
+            temp.push_back(i);
+            up[i]=adj[i].begin()->first;
         }
-        else 
-        {
-            ll valy = brr[y];
-            auto it1 = bm[valy];
-            auto it2 = it1;
-            bm.erase(valy);
-
-            
-            ll oi = it1[1]+it1[0]-1;
-            ll oival = min(br[oi][0],ar[oi][0]);
-            res = (res*qexp(oival,MOD-2,MOD))%MOD;
-            
-            it1[0]--;
-            if (it1[0]>0) bm[valy]=it1;
-            it2 = bm[valy+1];
-            bm[valy+1]={it2[0]+1,it1[0]+it1[1]};
-            brr[y]++;
-            
-            it2 = bm[valy+1];
-            ll oj = it2[1];
-            br[oj][0]=valy+1;
-            ll ojval = min(br[oj][0],ar[oj][0]);
-            res = (res*ojval)%MOD;
-        }
-        ans = res;
-        cout << res << " ";
     }
-    cout << endl;
+    int id=0;
+    vector<ar<int,2>> cnt(n,{0,0});
+    while(!temp.empty())
+    {
+        vector<int> temp2;
+        for (int u:temp)
+        {
+            vis[u]=1;
+            cnt[up[u]][id]++;
+            adj[up[u]].erase(u);
+        }
+        for (int u:temp)
+        {
+            if (vis[up[u]]==0) temp2.push_back(up[u]);
+            vis[up[u]]=1;
+            if (adj[up[u]].size()==1) up[up[u]]=adj[up[u]].begin()->first;
+        }
+        temp = temp2;
+        id=1-id;
+    }
+
+    
 }
 
 signed main() {
