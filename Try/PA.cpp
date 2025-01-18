932d672066f7e60fed9edc85fe9986616ed0895e
Pirate-Emperor <piratekingrahul@gmail.com>
2025-01-19 02:11:06 +0530

    18-01-25

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -43,126 +43,44 @@ void solve() {
     ll a=0,b=0,c=0,d=0;
     ll g=0,q=0;
     cin >> n >> m;
-    string s;
-    cin >> s;
-    vector<pair<int,int>> path;
-    map<ar<int,2>,int> mpi;
-    a=0;
-    b=0;
-    path.push_back({a,b});
-    mpi[{a,b}]=0;
-    for (int i=0;i<n+m-2;i++)
-    {
-        if (s[i]=='D') a++;
-        else b++;
-        path.push_back({a,b});
-        mpi[{a,b}]=i+1;
-    }
-    vector<int> sola(n+m-1,0);
-    vector<vector<int>> arr(n,vector<int>(m));
-    int sum=0;
-    vector<int> col(m,0), row(n,0);
+    ll res=0;
+    vector<int> arr(n);
+    vector<int> brr(n);
+    a=m/n;
+    map<ar<int,3>,int> mpi;
+    int lef=m;
     for (int i=0;i<n;i++)
     {
-        for (int j=0;j<m;j++)
-        {
-            cin >> arr[i][j];
-            sum+=arr[i][j];
-            row[i]+=arr[i][j];
-            col[j]+=arr[i][j];
-        }
+        cin >> arr[i];
+        brr[i]=floor(sqrt(a/arr[i]));
+        int cost = brr[i]*brr[i]*arr[i];
+        lef-=cost;
+        int ncost = (brr[i]+1)*(brr[i]+1)*arr[i];
+        mpi[{ncost,brr[i],i}]=brr[i];
     }
- 
-    vector<ar<int,2>> inter(n+m-1, {LINF,LINF});
-    a=0;
-    b=0;
     
-    for (int i=0;i<n+m-2;i++)
+    while(true)
     {
-        if (s[i]=='D') 
-        {
-            ar<int,2> temp = {0,0};
-            for (int j=0;j<m;j++)
-            {
-                if (j==b) break;
-                if (mpi.find({a,j})!=mpi.end()) 
-                {
-                    temp[0] += inter[mpi[{a,j}]][0];
-                    temp[1] += inter[mpi[{a,j}]][1];
-                }
-            }
-            ar<int,2> nex = {1-temp[0],-row[a]-temp[1]};
-            inter[mpi[{a,b}]]=nex;
-            a++;
-            
-        }
-        else 
-        {
-            ar<int,2> temp = {0,0};
-            for (int j=0;j<n;j++)
-            {
-                if (j==a) break;
-                if (mpi.find({j,b})!=mpi.end()) 
-                {
-                    temp[0] += inter[mpi[{j,b}]][0];
-                    temp[1] += inter[mpi[{j,b}]][1];
-                }
-            }
-            ar<int,2> nex = {1-temp[0],-col[b]-temp[1]};
-            inter[mpi[{a,b}]]=nex;
-            b++;
-            
-        }
-    }
-    ar<int,2> temp = {0,0};
-    for (int j=0;j<m;j++)
-    {
-        if (j==b) break;
-        if (mpi.find({a,j})!=mpi.end()) 
-        {
-            temp[0] += inter[mpi[{a,j}]][0];
-            temp[1] += inter[mpi[{a,j}]][1];
-        }
-    }
-    ar<int,2> nex = {1-temp[0],-row[a]-temp[1]};
-    inter[mpi[{a,b}]]=nex;
+        int cost = mpi.begin()->first[0];
+        auto it = mpi.begin()->first;
+        auto it2 = mpi.begin()->second;
+        
+        it2++;
+        int ncost = arr[it[2]]*(it2+1)*(it2+1);
+        int def = arr[it[2]]*(2*it2-1);
+        cout << it[2] << " " << it[1] << endl;
+        if (def>lef) break;
+        
+        mpi.erase(it);
+        mpi[{ncost,it2,it[1]}]=it2;
+        lef-=def;
 
-    temp = {0,0};
-    for (int j=0;j<n;j++)
-    {
-        if (j==a) break;
-        if (mpi.find({j,b})!=mpi.end()) 
-        {
-            temp[0] += inter[mpi[{j,b}]][0];
-            temp[1] += inter[mpi[{j,b}]][1];
-        }
     }
-    nex = {1-temp[0],-col[b]-temp[1]};
-    nex[0]-=inter[mpi[{a,b}]][0];
-    nex[1]-=inter[mpi[{a,b}]][1];
-
-    int val = 0;
-    if (nex[0]!=0) val = -nex[1]/nex[0];
-
-    // cout << val << endl;
-    for (int i=0;i<n+m-1;i++)
+    for (auto it: mpi)
     {
-        // cout << inter[i][0] << " " << inter[i][1] << " | ";
-        sola[i]=inter[i][0]*val+inter[i][1];
-        int a = path[i].first;
-        int b = path[i].second;
-        arr[a][b]=sola[i];
+        res+=it.second;
     }
-    for (int i=0;i<n;i++)
-    {
-        for (int j=0;j<m;j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    
+    cout << res << endl;
     
 }
 
@@ -170,7 +88,7 @@ signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
