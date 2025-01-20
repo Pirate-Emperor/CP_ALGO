be63f435b38355737f3d8e33b382c70397d417bc
Pirate-Emperor <piratekingrahul@gmail.com>
2025-01-20 23:05:48 +0530

    20-01-25

--- Try/PA.cpp
+++ Try/PA.cpp
@@ -34,53 +34,51 @@ ll qexp(ll a, ll b, ll m) {
     }
     return res;
 }
-vector<int> adj[MAX_N];
-int vis[MAX_N];
 
 void solve() {
     ll n=0,m=0,l=0,r=0,k=0;
     ll w=0,x=0,y=0,z=0;
     ll a=0,b=0,c=0,d=0;
     ll g=0,q=0;
-    cin >> n >> m;
+    string s, t;
+    cin >> s >> t;
+    n = s.size();
     ll res=0;
-    vector<int> arr(n);
-    vector<int> brr(n);
-    a=m/n;
-    map<ar<int,3>,int> mpi;
-    int lef=m;
+    vector<ar<int,2>> arr,brr;
     for (int i=0;i<n;i++)
     {
-        cin >> arr[i];
-        brr[i]=floor(sqrt(a/arr[i]));
-        int cost = brr[i]*brr[i]*arr[i];
-        lef-=cost;
-        int ncost = (brr[i]+1)*(brr[i]+1)*arr[i];
-        mpi[{ncost,brr[i],i}]=brr[i];
+        a+=s[i]=='0';
+        b+=t[i]=='0';
+    }
+    if (a!=b)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    chat ct = s[0];
+    c=1;
+    for (int i=1;i<n;i++)
+    {
+        if (ct!=s[i])
+        {
+            arr.push_back({c,ct-'0'});
+            c=1;
+        }
+        c++;
+    }
+    ct = t[0];
+    c=1;
+    for (int i=1;i<n;i++)
+    {
+        if (ct!=t[i])
+        {
+            brr.push_back({c,ct-'0'});
+            c=1;
+        }
+        c++;
     }
     
-    while(true)
-    {
-        int cost = mpi.begin()->first[0];
-        auto it = mpi.begin()->first;
-        auto it2 = mpi.begin()->second;
-        
-        it2++;
-        int ncost = arr[it[2]]*(it2+1)*(it2+1);
-        int def = arr[it[2]]*(2*it2-1);
-        cout << it[2] << " " << it[1] << endl;
-        if (def>lef) break;
-        
-        mpi.erase(it);
-        mpi[{ncost,it2,it[1]}]=it2;
-        lef-=def;
-
-    }
-    for (auto it: mpi)
-    {
-        res+=it.second;
-    }
-    cout << res << endl;
+    return;
     
 }
 
@@ -88,7 +86,7 @@ signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    // cin >> tc;
+    cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
